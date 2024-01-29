baead84f110b3b4b2eeb77cae76f08a4687a1f8b
narian18 <narianmoneaunaidu@gmail.com>
2024-01-29 21:22:22 +0200

    45-minutes in - login API exists but no logic in it

---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.demo.security.config;
+
+import com.example.demo.appUser.AppUserService;
+import com.example.demo.security.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final PasswordEncoder passwordEncoder;
+
+    public WebSecurityConfig(AppUserService appUserService, PasswordEncoder passwordEncoder) {
+        this.appUserService = appUserService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v*/registration/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                    // .loginPage("/login")
+                    // .loginProcessingUrl("/perform_login")
+                     .defaultSuccessUrl("/", true)
+                    .permitAll()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.NOT_FOUND)))
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder.bCryptPasswordEncoder());
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+}
